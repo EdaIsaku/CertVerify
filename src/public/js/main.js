1976227743bcc06f919aa6c10413fd91638c6b87
Eda <eda.isaku@yahoo.com>
2023-08-27 10:43:47 +0200

    ADD: manifest file & activate service wokers

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,3 +1,26 @@
+//pwa- registrate service worker
+
+const registrateServiceWorker = async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        '../serviceWorker.js'
+      );
+      if (registration.installing) {
+        console.log('Service worker installing');
+      } else if (registration.waiting) {
+        console.log('Service worker installed');
+      } else if (registration.active) {
+        console.log('Service worker active');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+registrateServiceWorker();
+
 const getElement = (selector) => {
   return document.querySelector(selector);
 };
